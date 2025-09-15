9abdd3517d7799201f07c8718a31f798bdcd24ac
Andi <150683300+rAndi00@users.noreply.github.com>
2025-09-15 23:07:57 +0100

    Adding Sanctum( For roles and perrmissions) Also adding User Model

--- clinic-api/app/Models/User.php
+++ clinic-api/app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -20,7 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
+        'preferred_language',
     ];
 
     /**
@@ -34,7 +36,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
      * @return array<string, string>
      */
@@ -45,4 +47,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Default values for attributes.
+     *
+     * @var array<string, mixed>
+     */
+    protected $attributes = [
+        'preferred_language' => 'en', // default to English
+    ];
 }
